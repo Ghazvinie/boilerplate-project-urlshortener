7eaf2c3b7d468eaf8b6985b668c154f657554034
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-05-30 18:00:06 +0100

    added urlValidator back

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -6,31 +6,37 @@ const createRandomString = require('../utils/randomString');
 async function getURL(req, res) {
     const short_url = req.params.short_url;
 
-    await URLModel.findOne({ shortURL: short_url }, (error, urlDoc) => {
-        if (error) {
-            console.log('Error searching database');
-        } else {
-            if (urlDoc === null) {
-                res.status(404).json('URL not found');
+    try {
+        await URLModel.findOne({ shortURL: short_url }, (error, urlDoc) => {
+            if (error) {
+                console.log('Error searching database');
             } else {
-                res.status(302).redirect(urlDoc.originalURL);
+                if (urlDoc === null) {
+                    res.status(404).json('URL not found');
+                } else {
+                    res.status(302).redirect(urlDoc.originalURL);
+                }
             }
-        }
-    });
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Handle POST reqs
 async function postURL(req, res) {
+    const shortURL = createRandomString();
+    const originalURL = res.locals.originalURL;
+    
     try {
-        const shortURL = createRandomString();
-        await URLModel.create({ originalURL: res.locals.originalURL, shortURL });
-        res.status(200).render('shorturl', { originalURL: res.locals.originalURL, shortURL, message: 'Your shortened URL!' });
+        await URLModel.create({ originalURL, shortURL });
+        res.status(200).render('shorturl', { originalURL, shortURL, message: 'Your shortened URL!' });
     } catch (error) {
         const errors = handleErrors(error);
         if (errors.error === 'URL has already been shortened') {
-            URLModel.findOne({ originalURL: res.locals.originalURL }, (error, doc) => {
+            URLModel.findOne({ originalURL }, (error, doc) => {
                 if (error) console.log(error);
-                return res.status(200)('shortURL', { originalURL: doc.originalURL, shortURL: doc.shortURL, message: 'URL has already been shortened!' });
+                return res.status(200).render('shorturl', { originalURL: doc.originalURL, shortURL: doc.shortURL, message: 'URL has already been shortened!' });
             });
         }
     }
