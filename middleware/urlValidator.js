2b83f3c30749d3bd0742be36e114cc7b2524038d
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-16 19:53:42 +0100

    changed urlValidator

--- middleware/urlValidator.js
+++ middleware/urlValidator.js
@@ -1,17 +1,16 @@
 const dns = require('dns');
 
-function urlValidator (req, res, next) {
+function urlValidator(req, res, next) {
     const originalURL = req.body.url;
-    const trimmedURL = originalURL.trim().toLowerCase().split('//');
-    dns.lookup(trimmedURL[trimmedURL.length - 1], (error) => {
+    const { hostname } = new URL(originalURL);
+    dns.lookup(hostname, (error) => {
         if (error) {
-            res.status(400).json({error: 'invalid url'});
+            res.status(400).json({ error: 'invalid url' });
         } else {
             res.locals.originalURL = originalURL;
-            res.locals.trimmedURL = trimmedURL;
             next();
         }
     });
 }
 
-module.exports = urlValidator; 
+module.exports = urlValidator;
